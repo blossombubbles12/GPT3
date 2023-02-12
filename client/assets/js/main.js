17b360453b3a75c23792eb0446ed590e2c3b437c
blossombubbles12 <113446418+blossombubbles12@users.noreply.github.com>
2023-02-12 19:02:54 +0100

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://gpt4.onrender.com', {
+  const response = await fetch('https://gpt3-oauf.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
